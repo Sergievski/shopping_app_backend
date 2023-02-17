b83f90a8ca2c9a0e4fd6ef1a01609d4774941de0
Stas.S <stas.eco82@gmail.com>
2023-02-17 02:14:12 +0200

    repear collectstatic problems

--- shopping/settings.py
+++ shopping/settings.py
@@ -178,6 +178,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
